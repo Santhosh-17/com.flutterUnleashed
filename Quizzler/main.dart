0764bdbe4e08833901691df276ae9acb6f0a0b18
Santhosh Govind <51264445+Santhosh-17@users.noreply.github.com>
2021-02-28 21:35:15 +0530

    Add files via upload

--- Quizzler/main.dart
+++ Quizzler/main.dart
@@ -6,11 +6,25 @@ class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[900],
+          title: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text('Quizzler',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0
+            ),
+            ),
+          ),
+
+        ),
         body: SafeArea(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
             child: QuizPage(),
           ),
         ),
@@ -28,70 +42,66 @@ class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
+      children: [
         Expanded(
-          flex: 5,
+          flex: 7,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
+                child: Text('The Captain of India Cricket Team is Virat Kohli',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
             ),
           ),
         ),
+
         Expanded(
+          flex: 1,
           child: Padding(
-            padding: EdgeInsets.all(15.0),
+            padding: const EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 15.0),
             child: FlatButton(
-              textColor: Colors.white,
+              onPressed: (){
+
+              },
+              child: Text('True',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white
+              ),
+              ),
               color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-                //The user picked true.
-              },
+              textColor: Colors.white,
             ),
           ),
         ),
+
         Expanded(
+          flex: 1,
           child: Padding(
-            padding: EdgeInsets.all(15.0),
+            padding: const EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 30.0),
             child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
+              onPressed: (){
+
+              },
+              child: Text('False',
                 style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+                    fontSize: 20.0,
+                    color: Colors.white
                 ),
               ),
-              onPressed: () {
-                //The user picked false.
-              },
+              color: Colors.red,
+              textColor: Colors.white,
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+
+
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
