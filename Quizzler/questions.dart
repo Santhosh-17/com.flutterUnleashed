7c23ae027d37674c86551003ef0aabb2240427de
Santhosh Govind <51264445+Santhosh-17@users.noreply.github.com>
2021-03-13 23:44:13 +0530

    Constructor Changed

--- Quizzler/questions.dart
+++ Quizzler/questions.dart
@@ -3,11 +3,6 @@ class Questions{
   String questionText ;
   bool questionAns;
 
-  Questions(String q,bool ans){
-    questionText = q;
-    questionAns = ans;
-  }
-
-
+  Questions(this.questionText,this.questionAns);
 
 }
