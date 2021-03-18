eee733b99fcc70c37b0fddfecf692a17879d4fb6
Santhosh Govind <51264445+Santhosh-17@users.noreply.github.com>
2021-03-18 16:38:22 +0530

    Updated

--- Destini/main.dart
+++ Destini/main.dart
@@ -1,6 +1,5 @@
+import 'package:destini/story_brain.dart';
 import 'package:flutter/material.dart';
-
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
 
 void main() => runApp(Destini());
 
@@ -14,7 +13,7 @@ class Destini extends StatelessWidget {
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+StoryBrain storyBrain = StoryBrain();
 
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
@@ -41,8 +40,7 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
+                    storyBrain.getStory(),
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
@@ -55,11 +53,11 @@ class _StoryPageState extends State<StoryPage> {
                   onPressed: () {
                     //Choice 1 made by user.
                     //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    storyBrain.nextStory(1);
                   },
                   color: Colors.red,
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    storyBrain.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -77,11 +75,11 @@ class _StoryPageState extends State<StoryPage> {
                   onPressed: () {
                     //Choice 2 made by user.
                     //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    storyBrain.nextStory(2);
                   },
                   color: Colors.blue,
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
+                    storyBrain.getChoice2(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
--- Destini/story_brain.dart
+++ Destini/story_brain.dart
@@ -2,6 +2,8 @@ import 'story.dart';
 
 class StoryBrain{
 
+  int _storyNum = 0;
+
   List<Story> _storyData = [
     Story(
         storyTitle:
@@ -34,29 +36,40 @@ class StoryBrain{
         choice2: '')
   ];
 
-  String getStory(int num){
-    return _storyData[num].storyTitle;
+  void nextStory(int userChoice){
+    if(_storyNum == 0 && userChoice == 1){
+      _storyNum = 2;
+    }else if(_storyNum == 0 && userChoice == 2){
+      _storyNum = 1;
+    }else if(_storyNum == 1 && userChoice == 1){
+      _storyNum = 2;
+    }else if(_storyNum == 1 && userChoice == 2){
+      _storyNum = 3;
+    }else if(_storyNum == 2 && userChoice == 1){
+      _storyNum = 5;
+    }else if(_storyNum == 2 && userChoice == 2){
+      _storyNum = 4;
+    }else{
+      _storyNum = 0;
+    }
+  }
+
+  String getStory(){
+    return _storyData[_storyNum].storyTitle;
+  }
+
+  String getChoice1(){
+    return _storyData[_storyNum].choice1;
+  }
+
+  String getChoice2(){
+    return _storyData[_storyNum].choice2;
   }
 
 }
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-
-//TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
-
-//TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
-
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
-//TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-
-//TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+//TODO: Step 27 - Create a method called buttonShouldBeVisible()
+// which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true
+// if that is the case, else it should return false.
