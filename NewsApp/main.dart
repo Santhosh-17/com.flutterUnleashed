4de626a9f67707b2802b93be7764c6ac3221ded9
Santhosh Govind <51264445+Santhosh-17@users.noreply.github.com>
2021-05-06 23:57:20 +0530

    main file added

---
+++ NewsApp/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/pages/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
