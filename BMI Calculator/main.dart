d361b67422a7f66881a7016a33524624588b4d8d
Santhosh Govind <51264445+Santhosh-17@users.noreply.github.com>
2021-05-10 23:16:14 +0530

    Added themeData to main file

--- BMI Calculator/main.dart
+++ BMI Calculator/main.dart
@@ -1,35 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'pages/input_page.dart';
+
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        textTheme: TextTheme(
+         bodyText2: TextStyle(
+           fontSize: 18,
+         )
+        )
+      ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
